015993aee3107e1711cad066ed6b8d15a93597f2
chenhao1umbc <chenhao1@umbc.edu>
2023-06-29 15:45:38 -0400

    redo data split to avoid cheeting

--- data_process.py
+++ data_process.py
@@ -2,7 +2,7 @@
 from utils import *
 
 #%%
-neg, pos = torch.load('./data/neg_pos.pt')
+neg_all, pos_all = torch.load('./data/neg_pos.pt')
 def my_chuck(x, chunks=2, dim=0):
     return torch.stack(torch.chunk(x,chunks,dim), dim=1)
 
@@ -41,8 +41,17 @@ def show_data(data, n):
     plt.tight_layout()  # Adjust the layout to prevent overlapping
     plt.show()
 
-show_data(neg, 0)
-show_data(pos, 200)
+view_data = False
+if view_data:
+    show_data(neg_all, 0)
+    show_data(pos_all, 200)
+
+torch.manual_seed(1)
+n_tr_val = int(neg_all.shape[0]*0.85)
+ind = torch.randperm(neg_all.shape[0])
+neg_shuffle, pos_shuffle = neg_all[ind], pos_all[ind]
+pos, neg = neg_shuffle[:n_tr_val], pos_shuffle[:n_tr_val]
+torch.save( [neg_shuffle[n_tr_val:], pos_shuffle[n_tr_val:]], './data/test_neg_pos.pt' )
 
 #%% Get the augmented positive
 core = pos[:,12:20, 16:48, 16:48] # must contains tumor
@@ -72,7 +81,7 @@ if view_data:
 
     plt.tight_layout()  # Adjust the layout to prevent overlapping
     plt.show()
-torch.save(aug_pos, 'aug_pos.pt')
+torch.save(aug_pos, './data/aug_pos.pt')
 
 #%%  Get the augmented negative (still as positive)
 tokens = my_chuck(my_chuck(my_chuck(neg,2,-1), 2,-2), 4, -3).reshape(neg.shape[0],-1,8,32,32)
@@ -102,7 +111,7 @@ if view_data:
 
     plt.tight_layout()  # Adjust the layout to prevent overlapping
     plt.show()
-torch.save(aug_neg_core, 'aug_neg+core.pt')
+torch.save(aug_neg_core, './data/aug_neg+core.pt')
 
 #%%  Get the augmented negative (still as positive)
 aug_neg = my_chuck(my_chuck(my_chuck(neg,2,-1), 2,-2), 4, -3).reshape(neg.shape[0],-1,8,32,32)
@@ -131,4 +140,6 @@ if view_data:
 
     plt.tight_layout()  # Adjust the layout to prevent overlapping
     plt.show()
-torch.save(aug_neg, 'aug_neg.pt')
+torch.save(aug_neg, './data/aug_neg.pt')
+
+#%%
