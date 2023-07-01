75f28e6b2dad753e3c23761bfa1e7f67326a8fa6
chenhao1umbc <chenhao1@umbc.edu>
2023-07-01 12:27:44 -0400

    finished pca code

--- pca.py
+++ pca.py
@@ -76,16 +76,16 @@ K = 500
 un, s2n = pca(dn)
 up, s2p = pca(dp) # check how many dim to choose by s2q
 proj_n = un[:, :K].t()[None, None]
-mean_n = dn.mean(1)
+mean_n = dn.mean(1, keepdim=True)
 proj_p = up[:, :K].t()[None, None]
-mean_p = dp.mean(1)
+mean_p = dp.mean(1, keepdim=True)
 
 test_neg_tokens, test_pos_tokens = tokenize(test_neg), tokenize(test_pos) #[n, 16, 8, 32, 32]
-tnt = test_neg_tokens.reshape(982, 16, -1, 1)
-tpt = test_pos_tokens.reshape(982, 16, -1, 1)
+tnt = test_neg_tokens.reshape(174, 16, -1, 1)
+tpt = test_pos_tokens.reshape(174, 16, -1, 1)
 
-res1 = (proj_n @ (tnt - mean_n)).squeeze() #[n, 16, K]
-res2 = (proj_p @ (tnt - mean_p)).squeeze()
+res1 = (proj_n @ (tnt - mean_n)).squeeze().norm(dim=-1) #[n, 16, K]
+res2 = (proj_p @ (tnt - mean_p)).squeeze().norm(dim=-1)
 
 """theoretically one positive in the token is postive
 but then the false alarm rate will be too high.
@@ -95,12 +95,11 @@ thre = 5
 res = res1>res2
 acc_n = (res.sum(1) <= thre).sum()
 
-
-res1 = (proj_n @ (tpt - mean_n)).squeeze() #[n, 16, K]
-res2 = (proj_p @ (tpt - mean_p)).squeeze() 
+res1 = (proj_n @ (tpt - mean_n)).squeeze().norm(dim=-1) #[n, 16, K]
+res2 = (proj_p @ (tpt - mean_p)).squeeze().norm(dim=-1)
 res = res2>res1
 acc_p = (res.sum(1) >= 11).sum()
 
 acc = (acc_n + acc_p)/(test_neg.shape[0]*2) 
-print(f'acc is {acc.item()}')
+print(f'acc is {acc.item()}') #0.9224
 
