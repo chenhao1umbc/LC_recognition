a3a9ee73da98fb9016c85b067393d606639b9fad
chenhao1umbc <chenhao1@umbc.edu>
2023-07-09 00:47:10 -0400

    fixed important bugs in augmented data generation

--- data_process.py
+++ data_process.py
@@ -50,14 +50,14 @@ torch.manual_seed(1)
 n_tr_val = int(neg_all.shape[0]*0.85)
 ind = torch.randperm(neg_all.shape[0])
 neg_shuffle, pos_shuffle = neg_all[ind], pos_all[ind]
-pos, neg = neg_shuffle[:n_tr_val], pos_shuffle[:n_tr_val]
+neg, pos = neg_shuffle[:n_tr_val], pos_shuffle[:n_tr_val]
 torch.save( [neg_shuffle[n_tr_val:], pos_shuffle[n_tr_val:]], './data/test_neg_pos.pt' )
 torch.save( [neg_shuffle[:n_tr_val], pos_shuffle[:n_tr_val]], './data/tr_val_neg_pos.pt' )
 
 #%% Get the augmented positive
-core = pos[:,12:20, 16:48, 16:48] # must contains tumor
+pcore = pos[:,12:20, 16:48, 16:48] # must contains tumor
 tokens = my_chuck(my_chuck(my_chuck(pos,2,-1), 2,-2), 4, -3).reshape(pos.shape[0],-1,8,32,32)
-aug_pos = tokens + core[:,None]
+aug_pos = tokens + pcore[:,None]
 
 view_data = False
 if view_data:
@@ -86,7 +86,7 @@ torch.save(aug_pos, './data/aug_pos.pt')
 
 #%%  Get the augmented negative (still as positive)
 tokens = my_chuck(my_chuck(my_chuck(neg,2,-1), 2,-2), 4, -3).reshape(neg.shape[0],-1,8,32,32)
-aug_neg_core = tokens + core[:,None]
+aug_neg_core = tokens + pcore[:,None]
 
 view_data = False
 if view_data:
@@ -112,10 +112,13 @@ if view_data:
 
     plt.tight_layout()  # Adjust the layout to prevent overlapping
     plt.show()
-torch.save(aug_neg_core, './data/aug_neg+core.pt')
+torch.save(aug_neg_core, './data/aug_neg+pcore.pt')
 
 #%%  Get the augmented negative (still as positive)
+ncore = pos[:,12:20, 16:48, 16:48] 
 aug_neg = my_chuck(my_chuck(my_chuck(neg,2,-1), 2,-2), 4, -3).reshape(neg.shape[0],-1,8,32,32)
+aug_neg = aug_neg + ncore[:,None]
+
 
 view_data = False
 if view_data:
---
+++ test_resnet.py
@@ -0,0 +1,78 @@
+#%%
+from utils import *
+# torch.autograd.set_detect_anomaly(True)
+
+#%% test on raw data
+"process data"
+test_neg, test_pos = torch.load('./data/test_neg_pos.pt') # data is not normalized
+test_neg = test_neg/(test_neg.abs().amax(dim=(1,2,3), keepdim=True) + 1e-5)
+test_pos = test_pos/(test_pos.abs().amax(dim=(1,2,3), keepdim=True) + 1e-5)
+
+n_test = test_neg.shape[0]
+d_test = torch.cat((test_neg, test_pos))
+l_test = torch.cat((torch.zeros(n_test), torch.ones(n_test)))
+data = Data.TensorDataset(d_test, l_test)
+te = Data.DataLoader(data, batch_size=64, shuffle=False)
+
+model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=False)
+model.conv1 = nn.Conv2d(8, 64, kernel_size=3, stride=1, padding=1, bias=False)
+model = torch.load('./res/resnet/best_resnet_pre.pt')
+
+model.eval()
+with torch.no_grad():
+    temp, acc = [], []
+    for i, (x, y) in enumerate(te):
+        x_cuda, y_cuda = x.cuda(), y.cuda()
+        y_hat = model(x_cuda).squeeze()
+        counter = ((y_hat.argmax(dim=-1)- y_cuda) == 0).sum()
+        acc.append(counter)
+acc_test = sum(acc)/d_test.shape[0]
+print('test acc is ', acc_test) # 0.7241
+
+
+#%% test on augmented data
+test_neg, test_pos = torch.load('./data/test_neg_pos.pt') # data is not normalized
+def tokenize(x):
+    """
+    x has the shape of [n, 32, 64, 64]
+    """
+    def my_chuck(x, chunks=2, dim=0):
+        return torch.stack(torch.chunk(x,chunks,dim), dim=1)
+    
+    tokens = my_chuck(my_chuck(my_chuck(x,2,-1), 2,-2), 4, -3).reshape(x.shape[0],-1,8,32,32)
+    return tokens
+
+test_neg_tokens, test_pos_tokens = tokenize(test_neg), tokenize(test_pos) #[n, 16, 8, 32, 32]
+core_neg = test_neg[:,12:20, 16:48, 16:48]
+core_pos = test_pos[:,12:20, 16:48, 16:48]
+test_neg_tokens = test_neg_tokens + core_neg[:,None]
+test_pos_tokens = test_pos_tokens + core_pos[:,None]
+test_neg = test_neg_tokens/(test_neg_tokens.abs().amax(dim=(1,2,3,4), keepdim=True) + 1e-5)
+test_pos = test_pos_tokens/(test_pos_tokens.abs().amax(dim=(1,2,3,4), keepdim=True) + 1e-5)
+
+n_test = test_neg.shape[0]
+d_test = torch.cat((test_neg, test_pos))
+l_test = torch.cat((torch.zeros(n_test), torch.ones(n_test)))
+data = Data.TensorDataset(d_test, l_test)
+te = Data.DataLoader(data, batch_size=64, shuffle=False)
+
+model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=False)
+model.conv1 = nn.Conv2d(8, 64, kernel_size=3, stride=1, padding=1, bias=False)
+model = torch.load('./res/resnet/best_resnet_aug.pt')
+
+threshold = 2
+model.eval()
+with torch.no_grad():
+    temp, acc = [], []
+    for i, (x, y) in enumerate(te):
+        x_cuda, y_cuda = x.reshape(-1,8,32,32).cuda(), y.cuda()
+        y_hat16 = model(x_cuda).squeeze()
+        y_hat = y_hat16.reshape(-1,16, 1000)
+        l_hat = y_hat.argmax(dim=-1).sum(dim=-1)
+        l_hat[l_hat<=threshold] = 0
+        l_hat[l_hat>threshold] = 1
+        counter = ((l_hat- y_cuda) == 0).sum()
+        acc.append(counter)
+        break
+acc_test = sum(acc)/d_test.shape[0]
+print('test acc is ', acc_test) 
